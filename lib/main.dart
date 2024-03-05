eb8bef02065d2559173dcf54f1b4f076dfb7eee0
MrKing-dev <91885325+MrKing-dev@users.noreply.github.com>
2024-03-05 13:16:16 -0500

    Added drop down menus to filter by last name and sport.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 var photo_displayed = '';
+String selectedLetter = '';
+String selectedSport = '';
 
 void main() => runApp(MyApp());
 
@@ -12,6 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.red,
+      ),
       home: HomeScreen(),
     );
   }
@@ -24,6 +30,10 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late List<Map<String, dynamic>> inducteeList;
+  late List<Map<String, dynamic>> filteredInducteeList;
+
+  final TextEditingController letterController = TextEditingController();
+  final TextEditingController sportController = TextEditingController();
 
   @override
   void initState() {
@@ -37,14 +47,56 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       inducteeList = List<Map<String, dynamic>>.from(jsonData);
       inducteeList.sort((a, b) => a['last_name'].compareTo(b['last_name']));
+      filteredInducteeList = List<Map<String, dynamic>>.from(inducteeList);
     });
+  }
+
+  void filterByLetter(String letter) {
+    setState(() {
+      if (selectedSport != '') {
+        filteredInducteeList = inducteeList
+            .where((inductee) =>
+                inductee['last_name'].startsWith(letter) &&
+                inductee['sports'].contains(selectedSport))
+            .toList();
+      } else {
+        filteredInducteeList = inducteeList
+            .where((inductee) => inductee['last_name'].startsWith(letter))
+            .toList();
+      }
+    });
+  }
+
+  void filterBySport(String sport) {
+    setState(() {
+      if (selectedLetter != '') {
+        filteredInducteeList = inducteeList
+            .where((inductee) =>
+                inductee['last_name'].startsWith(selectedLetter) &&
+                inductee['sports'].contains(sport))
+            .toList();
+      } else {
+        filteredInducteeList = inducteeList
+            .where((inductee) => inductee['sports'].contains(sport))
+            .toList();
+      }
+    });
+  }
+
+  int gridOutput() {
+    if (MediaQuery.of(context).size.width <
+        MediaQuery.of(context).size.height) {
+      return 3;
+    } else {
+      return 5;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 100,
+        //toolbarHeight: 100,
         backgroundColor: Colors.red,
         foregroundColor: Colors.white,
         title: Text(
@@ -55,7 +107,113 @@ class _HomeScreenState extends State<HomeScreen> {
             fontWeight: FontWeight.bold,
           ),
         ),
-        actions: [],
+        actions: [
+          IconButton(
+              iconSize: 40,
+              onPressed: () {
+                _loadInducteeList();
+              },
+              icon: Icon(Icons.refresh)),
+          SizedBox(width: 20),
+          DropdownMenu(
+            controller: letterController,
+            label: Text('Filter by Last Name'),
+            initialSelection: "A",
+            width: 200,
+            inputDecorationTheme: InputDecorationTheme(
+              constraints: BoxConstraints.tight(
+                Size.fromHeight(35),
+              ),
+              labelStyle: TextStyle(color: Colors.white),
+              hintStyle: TextStyle(color: Colors.white),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+            ),
+            dropdownMenuEntries: <DropdownMenuEntry>[
+              DropdownMenuEntry(value: "A", label: "A"),
+              DropdownMenuEntry(value: "B", label: "B"),
+              DropdownMenuEntry(value: "C", label: "C"),
+              DropdownMenuEntry(value: "D", label: "D"),
+              DropdownMenuEntry(value: "E", label: "E"),
+              DropdownMenuEntry(value: "F", label: "F"),
+              DropdownMenuEntry(value: "G", label: "G"),
+              DropdownMenuEntry(value: "H", label: "H"),
+              DropdownMenuEntry(value: "I", label: "I"),
+              DropdownMenuEntry(value: "J", label: "J"),
+              DropdownMenuEntry(value: "K", label: "K"),
+              DropdownMenuEntry(value: "L", label: "L"),
+              DropdownMenuEntry(value: "M", label: "M"),
+              DropdownMenuEntry(value: "N", label: "N"),
+              DropdownMenuEntry(value: "O", label: "O"),
+              DropdownMenuEntry(value: "P", label: "P"),
+              DropdownMenuEntry(value: "Q", label: "Q"),
+              DropdownMenuEntry(value: "R", label: "R"),
+              DropdownMenuEntry(value: "S", label: "S"),
+              DropdownMenuEntry(value: "T", label: "T"),
+              DropdownMenuEntry(value: "U", label: "U"),
+              DropdownMenuEntry(value: "V", label: "V"),
+              DropdownMenuEntry(value: "W", label: "W"),
+              DropdownMenuEntry(value: "X", label: "X"),
+              DropdownMenuEntry(value: "Y", label: "Y"),
+              DropdownMenuEntry(value: "Z", label: "Z"),
+            ],
+            onSelected: (letter) {
+              setState(() {
+                selectedLetter = letter;
+                filterByLetter(letter);
+              });
+            },
+          ),
+          SizedBox(width: 20),
+          DropdownMenu(
+            controller: sportController,
+            label: Text('Filter by Sport'),
+            initialSelection: "Football",
+            inputDecorationTheme: InputDecorationTheme(
+              constraints: BoxConstraints.tight(
+                Size.fromHeight(35),
+              ),
+              labelStyle: TextStyle(color: Colors.white),
+              hintStyle: TextStyle(color: Colors.white),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+            ),
+            width: 200,
+            dropdownMenuEntries: <DropdownMenuEntry>[
+              DropdownMenuEntry(value: "Football", label: "Football"),
+              DropdownMenuEntry(value: "Basketball", label: "Basketball"),
+              DropdownMenuEntry(value: "Volleyball", label: "Volleyball"),
+              DropdownMenuEntry(
+                  value: "Track and Field", label: "Track and Field"),
+              DropdownMenuEntry(value: "Baseball", label: "Baseball"),
+              DropdownMenuEntry(value: "Cross Country", label: "Cross Country"),
+              DropdownMenuEntry(value: "Soccer", label: "Soccer"),
+              DropdownMenuEntry(value: "Swimming", label: "Swimming"),
+              DropdownMenuEntry(value: "Wrestling", label: "Wrestling"),
+              DropdownMenuEntry(value: "Golf", label: "Golf"),
+              DropdownMenuEntry(value: "Tennis", label: "Tennis"),
+              DropdownMenuEntry(value: "Softball", label: "Softball"),
+              DropdownMenuEntry(value: "Field Hockey", label: "Field Hockey"),
+              DropdownMenuEntry(value: "Lacrosse", label: "Lacrosse"),
+              DropdownMenuEntry(value: "Cheerleading", label: "Cheerleading"),
+            ],
+            onSelected: (sport) {
+              setState(() {
+                selectedSport = sport;
+                filterBySport(sport);
+              });
+            },
+          ),
+          SizedBox(width: 20),
+        ],
       ),
       body: Container(
         decoration: BoxDecoration(
@@ -68,25 +226,30 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                'Athletics Hall of Fame',
-                style: GoogleFonts.poppins(
-                  color: Colors.white,
-                  fontSize: 75,
-                  fontWeight: FontWeight.bold,
+              FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  'Athletics Hall of Fame',
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 75,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
               Expanded(
                 child: GridView.builder(
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 5,
+                    crossAxisCount:
+                        gridOutput(), // Creates a 5 item wide grid, unless the orientation is vertical.
                   ),
-                  itemCount: inducteeList.length,
+                  itemCount: filteredInducteeList.length,
                   itemBuilder: (context, index) {
                     return Card(
                       clipBehavior: Clip.hardEdge,
                       color: Colors.red[900],
-                      margin: EdgeInsets.all(40),
+                      margin: EdgeInsets.all(
+                          MediaQuery.of(context).size.width * 0.02),
                       elevation: 5,
                       child: Stack(
                         children: [
@@ -95,7 +258,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             left: 0,
                             right: 0,
                             child: Image.asset(
-                              inducteeList[index]['profile_photo'],
+                              filteredInducteeList[index]['profile_photo'],
                               fit: BoxFit.cover,
                             ),
                           ),
@@ -123,10 +286,10 @@ class _HomeScreenState extends State<HomeScreen> {
                                   fit: BoxFit.scaleDown,
                                   child: Text(
                                     textAlign: TextAlign.left,
-                                    '${inducteeList[index]['first_name']} ${inducteeList[index]['last_name']}',
-                                    style: TextStyle(
+                                    '${filteredInducteeList[index]['first_name']} ${filteredInducteeList[index]['last_name']}',
+                                    style: GoogleFonts.poppins(
                                       color: Colors.white,
-                                      fontSize: 50,
+                                      fontSize: 40,
                                     ),
                                   ),
                                 ),
@@ -134,7 +297,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                   fit: BoxFit.scaleDown,
                                   child: Text(
                                     textAlign: TextAlign.left,
-                                    '${inducteeList[index]['sports'].join(', ')}',
+                                    '${filteredInducteeList[index]['sports'].join(', ')}',
                                     style: TextStyle(
                                         color: Colors.white, fontSize: 14),
                                   ),
@@ -149,7 +312,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                 context: context,
                                 builder: (BuildContext context) {
                                   return CardDetail(
-                                      inductee: inducteeList[index]);
+                                      inductee: filteredInducteeList[index]);
                                 },
                                 constraints: BoxConstraints(
                                     minWidth:
@@ -223,8 +386,10 @@ class _CardDetailState extends State<CardDetail> {
                   children: [
                     Text(
                       '${widget.inductee['first_name']} ${widget.inductee['last_name']}',
-                      style:
-                          TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                      style: GoogleFonts.poppins(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                     Text(
                       'Induction Year: ${widget.inductee['induction_year']}',
@@ -244,8 +409,10 @@ class _CardDetailState extends State<CardDetail> {
                     SizedBox(height: 10),
                     Text(
                       'Description',
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      style: GoogleFonts.poppins(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                     SizedBox(height: 10),
                     Text(widget.inductee['description'],
@@ -253,8 +420,10 @@ class _CardDetailState extends State<CardDetail> {
                     SizedBox(height: 10),
                     Text(
                       'Other Photos',
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      style: GoogleFonts.poppins(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
